ae35ea447329ca191a9cc50c88e10364125f425d
taehyun <kimtaehyun002@gmail.com>
2025-02-10 17:47:41 +0900

    glitch

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 //app.use(cors());
 app.use(
   cors({
-    origin: "https://pgm-llm-project.github.io/",
+    origin: "https://pgm-llm-project.github.io",
     methods: ["POST"],
     allowedHeaders: ["Content-Type"],
   })
